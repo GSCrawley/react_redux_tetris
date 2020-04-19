005ae2c521f174afb4e4aec7cb77b21d9f350e13
GSCrawley <gscrawley@gmail.com>
2020-04-19 09:32:10 -0700

    Added connection for grid board

--- react_redux_tetris/src/components/grid-board.js
+++ react_redux_tetris/src/components/grid-board.js
@@ -1,3 +1,7 @@
+import { connect } from 'react-redux'
+import { moveDown } from '../actions'
+import { shapes } from '../utils'
+
 import React, { Component } from 'react'
 import GridSquare from './grid-square'
 
@@ -7,16 +11,35 @@ class GridBoard extends Component {
 
   // generates an array of 18 rows, each containing 10 GridSquares.
   makeGrid() {
-    const grid = []
-    for (let row = 0; row < 18; row ++) {
-      grid.push([])
-      for (let col = 0; col < 10; col ++) {
-        grid[row].push(<GridSquare key={`${col}${row}`} color="1" />)
-      }
-    }
-
-    return grid
+    // collect properties mapped to props from state.
+    const { grid, shape, rotation, x, y } = this.props
+    // get the block which is the current shape the player is controlling
+    const block = shapes[shape][rotation]
+    const blockColor = shape
+    // map rows
+    return grid.map((rowArray, row) => {
+      // map columns
+      return rowArray.map((square, col) => {
+        // Find the block x and y on the shape grid
+        // By subtracting the x and y from the col and the row we get the position of the upper left corner of the block array as if it was superimposed over the main grid
+        const blockX = col - x
+        const blockY = row - y
+        let color = square
+        // Map current falling block to grid.
+        // For any squares that fall on the grid we need to look at the block array and see if there is a 1 in this case we use the block color.
+        if (blockX >= 0 && blockX < block.length && blockY >= 0 && blockY < block.length) {
+          color = block[blockY][blockX] === 0 ? color : blockColor
+        }
+        // Generate a unique key for every block
+        const k = row * grid[0].length + col;
+        // Generate a grid square
+        return <GridSquare
+                key={k}
+                color={color} />
+      })
+    })
   }
+
 
   // The components generated in makeGrid are rendered in div.grid-board
   render () {
@@ -28,4 +51,23 @@ class GridBoard extends Component {
   }
 }
 
-export default GridBoard
+const mapStateToProps = (state) => {
+  return {
+    grid: state.game.grid,
+    shape: state.game.shape,
+    rotation: state.game.rotation,
+    x: state.game.x,
+    y: state.game.y,
+    speed: state.game.speed,
+    isRunning: state.game.isRunning
+  }
+}
+
+
+const mapDispatchToProps = () => {
+  return {
+    moveDown
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps())(GridBoard)
