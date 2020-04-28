d3c87a1bcb45324f0d15ebdad9852a8976363ecb
GSCrawley <gscrawley@gmail.com>
2020-04-27 20:19:05 -0700

    added connection for controls

--- react_redux_tetris/src/components/controls.js
+++ react_redux_tetris/src/components/controls.js
@@ -1,33 +1,53 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { moveDown, moveLeft, moveRight, rotate } from '../actions'
 
 class Controls extends Component {
 
-    render() {
-      return (
-        <div className="controls">
-          {/* left */}
-          <button className="control-button" onClick={(e) => {
-  
-          }}>Left</button>
-  
-          {/* right */}
-          <button className="control-button" onClick={(e) => {
-  
-          }}>Right</button>
-  
-          {/* rotate */}
-          <button className="control-button" onClick={(e) => {
-  
-          }}>Rotate</button>
-  
-          {/* down */}
-          <button className="control-button" onClick={(e) => {
-  
-          }}>Down</button>
-  
-        </div>
-      )
-    }
+  render() {
+    const { isRunning } = this.props
+
+    return (
+      <div className="controls">
+        {/* left */}
+        <button className="control-button" onClick={(e) => {
+          if (!isRunning) { return }
+          this.props.moveLeft()
+        }}>Left</button>
+
+        {/* right */}
+        <button className="control-button" onClick={(e) => {
+          if (!isRunning) { return }
+          this.props.moveRight()
+        }}>Right</button>
+
+        {/* rotate */}
+        <button className="control-button" onClick={(e) => {
+          if (!isRunning) { return }
+          this.props.rotate()
+        }}>Rotate</button>
+
+        {/* down */}
+        <button className="control-button" onClick={(e) => {
+          if (!isRunning) { return }
+          this.props.moveDown()
+        }}>Down</button>
+
+      </div>
+    )
   }
-  
-  export default Controls
+  }
+  const mapStateToProps = (state) => {
+    return {
+        isRunning: state.game.isRunning
+    }
+}
+  const mapDispatchToProps = () => {
+    return {
+        moveRight,
+        moveLeft,
+        moveDown,
+        rotate
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps())(Controls)
