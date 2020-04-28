1537cd93074ec1080c29538c1ad2c5d8d3055001
GSCrawley <gscrawley@gmail.com>
2020-04-27 21:13:13 -0700

    Added connection for score board

--- react_redux_tetris/src/components/score-board.js
+++ react_redux_tetris/src/components/score-board.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import { pause, resume, restart } from '../actions'
 
 class ScoreBoard extends Component {
 
   render() {
+    const { isRunning, score, resume, pause, restart, gameOver } = this.props
+
     return (
       <div className="score-board">
-        <div>Score:{ this.props.score }</div>
+        <div>Score:{ score }</div>
         <div>Level: 1</div>
 
         <button className="score-board-button" onClick={(e) => {
-
-        }}>Play</button>
+          if (gameOver) { return }
+          isRunning ? pause() : resume()
+        }}>{isRunning ? "Pause" : "Resume"}</button>
 
         <button className="score-board-button" onClick={(e) => {
-
+          restart()
         }}>Restart</button>
 
       </div>
@@ -21,4 +27,20 @@ class ScoreBoard extends Component {
   }
 }
 
-export default ScoreBoard
+const mapStateToProps = (state) => {
+  return {
+    score: state.game.score,
+    isRunning: state.game.isRunning,
+    gameOver: state.game.gameOver
+  }
+}
+
+const mapDispatchToProps = () => {
+  return {
+    pause,
+    resume,
+    restart
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps())(ScoreBoard)
