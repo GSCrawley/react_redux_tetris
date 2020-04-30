a171211b9b4c00e6f6641df5f11b16505792ce61
GSCrawley <gscrawley@gmail.com>
2020-04-29 18:27:37 -0700

    restart works

--- react_redux_tetris/src/reducers/game_reducer.js
+++ react_redux_tetris/src/reducers/game_reducer.js
@@ -86,8 +86,7 @@ const gameReducer = (state = defaultState(), action) => {
         return state
   
       case RESTART:
-  
-        return state
+        return defaultState()
   
       default:
         return state
