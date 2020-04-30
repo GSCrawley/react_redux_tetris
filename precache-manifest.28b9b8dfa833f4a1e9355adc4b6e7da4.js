ddde6aaa266dc8a3554b992af3bcc5e63b9739c7
GSCrawley <gscrawley@gmail.com>
2020-04-29 19:03:41 -0700

    Updates

---
+++ precache-manifest.28b9b8dfa833f4a1e9355adc4b6e7da4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ec875880e6bcf92fd7c871f3185cd01",
+    "url": "/react_redux_tetris/index.html"
+  },
+  {
+    "revision": "e2774b381c048c98c8f6",
+    "url": "/react_redux_tetris/static/css/main.8bcd2ff4.chunk.css"
+  },
+  {
+    "revision": "eadb5ba9e9eff5685a56",
+    "url": "/react_redux_tetris/static/js/2.d1620106.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_redux_tetris/static/js/2.d1620106.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2774b381c048c98c8f6",
+    "url": "/react_redux_tetris/static/js/main.3bc6ae9a.chunk.js"
+  },
+  {
+    "revision": "fc8791ccbb954d9c0566",
+    "url": "/react_redux_tetris/static/js/runtime-main.d0187506.js"
+  }
+]);
